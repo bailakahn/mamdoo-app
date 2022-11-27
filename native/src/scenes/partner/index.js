97367fef21d44b40d2f6d5dde01e55992300790f
bailakahn <bailakahn@yahoo.fr>
2022-11-26 19:48:35 -0500

    Move upload screens under right folder

--- native/src/scenes/partner/index.js
+++ native/src/scenes/partner/index.js
@@ -1,14 +1,14 @@
 // HOME
 export { default as HomeScene } from "./home";
 export { default as Ride } from "./home/Ride";
-export { default as NotActiveScene } from "./home/NotActive";
-export { default as UploadScene } from "./home/Upload";
-export { default as UploadInstructionsScene } from "./home/UploadInstructions";
 
 // UPLOAD
-export { default as ProfilePictureScene } from "./home/Documents/ProfilePicture";
-export { default as DriverLicenseScene } from "./home/Documents/DriverLicense";
-export { default as CabLicenseScene } from "./home/Documents/CabLicense";
+export { default as NotActiveScene } from "./upload/NotActive";
+export { default as UploadScene } from "./upload/Upload";
+export { default as ProfilePictureScene } from "./upload/Documents/ProfilePicture";
+export { default as DriverLicenseScene } from "./upload/Documents/DriverLicense";
+export { default as CabLicenseScene } from "./upload/Documents/CabLicense";
+export { default as UploadInstructionsScene } from "./upload/UploadInstructions";
 
 // ACCOUNT
 export { default as AccountScene } from "./account";
