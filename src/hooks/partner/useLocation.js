731b7d080b587cb5f632bd2811099da132680664
bailakahn <bailakahn@yahoo.fr>
2023-01-06 03:23:44 +0000

    Makes sure only prod asks for background permissions

--- src/hooks/partner/useLocation.js
+++ src/hooks/partner/useLocation.js
@@ -12,122 +12,117 @@ const TASK_FETCH_LOCATION = "TASK_FETCH_LOCATION";
 var request = null;
 var partner = null;
 export default function useLocation(partner) {
-    const getRequest = useApi();
-    request = getRequest;
-    partner = partner;
-    const [location, setLocation] = useState(null);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [statusForeground, requestForegroundPermission] =
-        Location.useForegroundPermissions();
-    const [statusBackground, requestBackgroundPermission] =
-        Location.useBackgroundPermissions();
+  const getRequest = useApi();
+  request = getRequest;
+  partner = partner;
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [statusForeground, requestForegroundPermission] =
+    Location.useForegroundPermissions();
+  const [statusBackground, requestBackgroundPermission] =
+    Location.useBackgroundPermissions();
 
-    useEffect(() => {
-        requestForegroundPermission();
+  useEffect(() => {
+    requestForegroundPermission();
 
-        if (statusForeground?.status === Location.PermissionStatus.GRANTED)
-            Location.getCurrentPositionAsync({}).then(
-                ({ coords: { latitude, longitude } }) => {
-                    setLocation({ latitude, longitude });
-                }
-            );
-        return () => {};
-    }, []);
-
-    useEffect(() => {
-        if (statusForeground) {
-            if (statusForeground.status === Location.PermissionStatus.GRANTED) {
-                if (ENV_NAME !== "localhost") requestBackgroundPermission();
-
-                getCurrentPosition();
-            } else if (
-                statusForeground.status === Location.PermissionStatus.DENIED
-            ) {
-                setIsLoading(false);
-            }
+    if (statusForeground?.status === Location.PermissionStatus.GRANTED)
+      Location.getCurrentPositionAsync({}).then(
+        ({ coords: { latitude, longitude } }) => {
+          setLocation({ latitude, longitude });
         }
-    }, [statusForeground]);
+      );
+    return () => {};
+  }, []);
 
-    useEffect(() => {
-        if (
-            statusBackground &&
-            statusBackground.status === Location.PermissionStatus.GRANTED
-        )
-            Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
-                accuracy: Location.Accuracy.Highest,
-                distanceInterval: 1, // minimum change (in meters) betweens updates
-                timeInterval: 10000,
-                deferredUpdatesInterval: 1000, // minimum interval (in milliseconds) between updates
-                // foregroundService is how you get the task to be updated as often as would be if the app was open
-                foregroundService: {
-                    notificationTitle: "Using your location",
-                    notificationBody:
-                        "To turn off, go back to the app and switch something off."
-                }
-            });
+  useEffect(() => {
+    if (statusForeground) {
+      if (statusForeground.status === Location.PermissionStatus.GRANTED) {
+        if (ENV_NAME === "production") requestBackgroundPermission();
 
-        if (
-            statusForeground &&
-            statusForeground.status !== Location.PermissionStatus.UNDETERMINED
-        )
-            setIsLoading(false);
-    }, [statusBackground]);
+        getCurrentPosition();
+      } else if (statusForeground.status === Location.PermissionStatus.DENIED) {
+        setIsLoading(false);
+      }
+    }
+  }, [statusForeground]);
 
-    useEffect(() => {
-        if (
-            ENV_NAME === "localhost" &&
-            statusForeground &&
-            statusForeground.status !== Location.PermissionStatus.UNDETERMINED
-        )
-            setIsLoading(false);
-    }, [statusForeground]);
+  useEffect(() => {
+    if (
+      statusBackground &&
+      statusBackground.status === Location.PermissionStatus.GRANTED
+    )
+      Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
+        accuracy: Location.Accuracy.Highest,
+        distanceInterval: 1, // minimum change (in meters) betweens updates
+        timeInterval: 10000,
+        deferredUpdatesInterval: 1000, // minimum interval (in milliseconds) between updates
+        // foregroundService is how you get the task to be updated as often as would be if the app was open
+        foregroundService: {
+          notificationTitle: "Using your location",
+          notificationBody:
+            "To turn off, go back to the app and switch something off.",
+        },
+      });
 
-    const getCurrentPosition = async () => {
-        let {
-            coords: { latitude, longitude }
-        } = await Location.getCurrentPositionAsync({});
-        setLocation({ latitude, longitude });
+    if (
+      statusForeground &&
+      statusForeground.status !== Location.PermissionStatus.UNDETERMINED
+    )
+      setIsLoading(false);
+  }, [statusBackground]);
 
-        return { latitude, longitude };
-    };
+  useEffect(() => {
+    if (
+      ENV_NAME !== "production" &&
+      statusForeground &&
+      statusForeground.status !== Location.PermissionStatus.UNDETERMINED
+    )
+      setIsLoading(false);
+  }, [statusForeground]);
 
-    return {
-        location,
-        grantStatus: statusForeground?.status,
-        grantBackgroundStatus: statusBackground?.status,
-        error,
-        isLoading,
-        actions: { getCurrentPosition }
-    };
+  const getCurrentPosition = async () => {
+    let {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({});
+    setLocation({ latitude, longitude });
+
+    return { latitude, longitude };
+  };
+
+  return {
+    location,
+    grantStatus: statusForeground?.status,
+    grantBackgroundStatus: statusBackground?.status,
+    error,
+    isLoading,
+    actions: { getCurrentPosition },
+  };
 }
 
 TaskManager.defineTask(
-    TASK_FETCH_LOCATION,
-    async ({ data: { locations }, error }) => {
-        if (error) {
-            console.error(error);
-            return;
-        }
-        const [location] = locations;
-        try {
-            // console.log("Update Loacation", location.coords);
-
-            if (request && partner)
-                request({
-                    method: "POST",
-                    endpoint: "user/updateDriverLocation",
-                    params: {
-                        coordinates: [
-                            location.coords.longitude,
-                            location.coords.latitude
-                        ],
-                        type: "Point"
-                    }
-                });
-            // await axios.post(url, { location }); // you should use post instead of get to persist data on the backend
-        } catch (err) {
-            console.error(err);
-        }
+  TASK_FETCH_LOCATION,
+  async ({ data: { locations }, error }) => {
+    if (error) {
+      console.error(error);
+      return;
     }
+    const [location] = locations;
+    try {
+      // console.log("Update Loacation", location.coords);
+
+      if (request && partner)
+        request({
+          method: "POST",
+          endpoint: "user/updateDriverLocation",
+          params: {
+            coordinates: [location.coords.longitude, location.coords.latitude],
+            type: "Point",
+          },
+        });
+      // await axios.post(url, { location }); // you should use post instead of get to persist data on the backend
+    } catch (err) {
+      console.error(err);
+    }
+  }
 );
