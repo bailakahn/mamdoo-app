2b8a453b8106cf6d385230010ce750c1a6f4bdd4
bailakahn <bailakahn@yahoo.fr>
2023-02-01 15:38:22 +0000

    Improves background location updates

--- src/hooks/partner/useLocation.js
+++ src/hooks/partner/useLocation.js
@@ -4,6 +4,7 @@ import * as Location from "expo-location";
 import * as TaskManager from "expo-task-manager";
 import { useApi } from "_api";
 import { t2 } from "_utils/lang";
+import { useStore } from "_store";
 // import { ENV_NAME } from "@env";
 import Constants from "expo-constants";
 const ENV_NAME = Constants.expoConfig.extra.envName;
@@ -11,10 +12,15 @@ const ENV_NAME = Constants.expoConfig.extra.envName;
 const TASK_FETCH_LOCATION = "TASK_FETCH_LOCATION";
 var request = null;
 var partner = null;
-export default function useLocation(partner) {
+export default function useLocation() {
   const getRequest = useApi();
   request = getRequest;
-  partner = partner;
+  const {
+    auth: { partner: storePartner },
+  } = useStore();
+
+  partner = storePartner;
+
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -51,12 +57,12 @@ export default function useLocation(partner) {
     if (
       statusBackground &&
       statusBackground.status === Location.PermissionStatus.GRANTED
-    )
+    ) {
       Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
+        activityType: Location.ActivityType.AutomotiveNavigation,
         accuracy: Location.Accuracy.Highest,
-        distanceInterval: 1, // minimum change (in meters) betweens updates
-        timeInterval: 10000,
-        deferredUpdatesInterval: 1000, // minimum interval (in milliseconds) between updates
+        distanceInterval: 1000, // minimum change (in meters) betweens updates
+        deferredUpdatesInterval: 60000, // minimum interval (in milliseconds) between updates
         // foregroundService is how you get the task to be updated as often as would be if the app was open
         foregroundService: {
           notificationTitle: "Using your location",
@@ -64,6 +70,7 @@ export default function useLocation(partner) {
             "To turn off, go back to the app and switch something off.",
         },
       });
+    }
 
     if (
       statusForeground &&
@@ -120,7 +127,6 @@ TaskManager.defineTask(
             type: "Point",
           },
         });
-      // await axios.post(url, { location }); // you should use post instead of get to persist data on the backend
     } catch (err) {
       console.error(err);
     }
