e180f95e9d2a0a726e415bfff7eb5183e2328eb3
bailakahn <bailakahn@yahoo.fr>
2023-02-01 16:44:20 +0000

    Makes sure maintenance mode runs first

--- src/components/organisms/MaintenanceMode.js
+++ src/components/organisms/MaintenanceMode.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { View } from "react-native";
 import { useTheme, Text } from "react-native-paper";
 import { Classes } from "_styles";
-import { t } from "_utils/lang";
 export default function LocationDenied({ message }) {
   const { colors } = useTheme();
 
   return (
     <View style={Classes.container(colors)}>
       <View>
-        <Image
-          source={require("_assets/logo.png")}
-          style={Classes.formLogo(colors)}
-        />
+        <Text style={{ fontSize: 25, fontWeight: "bold", color: "#25C0D2" }}>
+          Mamdoo
+        </Text>
       </View>
       <View style={{ padding: 10 }}>
         <Text style={{ fontSize: 25, textAlign: "center" }}>{message}</Text>
